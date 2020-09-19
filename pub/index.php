71080c33b1aba53a625bdb65ed6d8604998dc5d5
Matej Malcic <matej.malcic3@gmail.com>
2020-09-19 12:53:35 +0200

    MVC setup

--- pub/index.php
+++ pub/index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo 'test';
+define('BP', dirname(__DIR__));
+
+spl_autoload_register(function ($class) {
+    $class = lcfirst($class);
+    $filename = BP . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+
+    if (file_exists($filename)) {
+        require_once $filename;
+    }
+});
+
+session_start();
+
+$router = new \App\Core\Router();
+$application = new \App\Core\Application($router);
+
+$response = $application->run();
+
+if ($response) {
+    echo $response;
+}
